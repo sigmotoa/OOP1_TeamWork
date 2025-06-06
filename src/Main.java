18d5a4f9b5853e5c4111050baaa86743977aed2b
Jean PauÃl <jpalbap@udistrital.edu.co>
2025-06-06 12:57:59 -0500

    algun mensaje

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import javax.sound.midi.Soundbank;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -8,7 +10,11 @@ public class Main {
         pet1.breed = "Criollo";
         pet1.kind = Kind.Dog;
 
-
+        Pet juan082 = new Pet();
+        juan082.name = "Simon";
+        juan082.breed = "Golden Retriever";
+        juan082.kind = Kind.Dog;
+        System.out.println(juan082);
         System.out.println(pet1);
     }
 }
